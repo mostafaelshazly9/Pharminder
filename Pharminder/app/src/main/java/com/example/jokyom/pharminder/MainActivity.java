831161052f93639c4d728b42db1b273e67f1a55e
yomna9sabry <44530654+yomna9sabry@users.noreply.github.com>
2019-05-10 04:34:27 +0200

    pharminder android app

---
+++ Pharminder/app/src/main/java/com/example/jokyom/pharminder/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.jokyom.pharminder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.logging.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayAdapter<String>adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ListView lv=(ListView)findViewById(R.id.listViewMedicine);
+        ArrayList<String> arrayMedicine=new ArrayList<>();
+        arrayMedicine.addAll(Arrays.<String>asList(getResources().getStringArray(R.array.array_medicine)));
+         adapter=new ArrayAdapter<>(
+                MainActivity.this,
+                android.R.layout.simple_list_item_1,
+                arrayMedicine);
+        lv.setAdapter(adapter);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu_search,menu);
+        MenuItem item =menu.findItem(R.id.menuSearch);
+        SearchView searchView=(SearchView)item.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
+    }
+}
