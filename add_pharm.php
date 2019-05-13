ddf8be99c24198a8efd357344713e6edc1bc6057
NadaKamal <nada.kamal.99.20.20@gmail.com>
2019-05-13 18:01:05 +0200

    Create add_pharm.php

---
+++ add_pharm.php
@@ -0,0 +1,21 @@
+<?php
+
+    
+    $Name = $_POST ["ud_name"];
+    $Address = $_POST ["ud_address"];
+    $LocLong = $_POST ["ud_latitude"];
+    $LocAtt = $_POST ["ud_longitude"];
+    $Phone = $_POST ["ud_phone"];
+    $Notes = $_POST ["ud_note"];
+
+$conn=mysqli_connect("localhost","root","","pharminder");
+
+$sql= "call newPharmacy ('$Name','$Address','$LocLong','$LocAtt','$Phone','$Notes')"; 
+    $result = mysqli_query($conn,$sql);
+
+ if ($result)
+    {
+         header('location: pharmaciesV3.php');
+    }
+  
+?>
